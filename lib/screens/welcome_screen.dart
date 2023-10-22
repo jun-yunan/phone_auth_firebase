4f7a006ca4a4c0a3623e47b45af75808d1a27024
jun-yunan <junyunan341695@gmail.com>
2023-10-22 19:05:26 +0700

    #1

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/provider/auth_provider.dart';
+import 'package:phone_auth_firebase/screens/home_screen.dart';
+import 'package:phone_auth_firebase/screens/register_screen.dart';
+import 'package:phone_auth_firebase/widgets/custom_button.dart';
+import 'package:provider/provider.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image1.png',
+                height: 300,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Let's get started",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Never a better time than now to start.",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: CustomButton(
+                  text: "Get started",
+                  onPressed: () async {
+                    if (ap.isSignedIn == true) {
+                      await ap.getDataFromSP().whenComplete(
+                            () => Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomeScreen(),
+                              ),
+                            ),
+                          );
+                    } else {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterScreen(),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
